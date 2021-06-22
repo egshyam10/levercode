5b2fc8c955c626c5e23846d168e8d3419af1a3b5
egshyam10 <35706505+egshyam10@users.noreply.github.com>
2021-06-22 13:28:23 +0530

    Delete ADP-Portal-API-Tracking-p/src directory

--- ADP-Portal-API-Tracking-p/src/main/java/com/accenture/dcsc/adp/portal/api/tracking/domain/core/NotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.accenture.dcsc.adp.portal.api.tracking.domain.core;
-
-@SuppressWarnings("serial")
-public class NotFoundException extends RuntimeException {
-
-    public NotFoundException(String message) {
-        super(message);
-    }
-}
